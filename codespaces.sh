85c39411a9fdc8bec221fb522e4162f3f18f9c8d
wangtong <wangtongbgi@163.com>
2024-12-05 09:50:50 +0800

    5th commit

--- codespaces.sh
+++ codespaces.sh
@@ -1,7 +1,5 @@
-本文主要介绍如何应用github codespaces来快速构建一个生物信息分析的环境。可以使用其下载生物软件，下载生物数据，使用docker，apptainer等。最后将数据传回自己的服务器内。
+# 配置devcontainer
 
-### 配置devcontainer
-```
 {
     "name": "Dev Container Python",
     "image": "mcr.microsoft.com/devcontainers/base:ubuntu-22.04",
@@ -22,41 +20,39 @@
     },
     "remoteUser": "root",
     "postCreateCommand": "uname -a"
-}
-```
 
-### Dockerfile设置
 
-#### 1、修改devcontainer.json
-```
+
+#1、修改devcontainer.json
+
 {
    "name": "Ubuntu",
    "build": { "dockerfile": "Dockerfile" },
 }
-```
 
 
-#### 2、修改Dockerfile
-```
+
+# 2、修改Dockerfile
+
 FROM ubuntu:jammy
 RUN apt update 
 RUN apt install -y git vim tree htop lftp tmux
 RUN echo 'export PS1="\[\e[31;1m\]\u\[\e[0m\] \[\e[32;1m\]\t \[\e[0m\]\[\e[34;1m\]\w\[\e[0m\]\n\[\e[31;1m\]$ \[\e[0m\]"' >>/root/.bashrc 
 RUN apt install -y bwa samtools bcftools ncbi-blast+
-```
 
-### 切换管理员账号
-```
+
+# 切换管理员账号
+
 #切换root
 su -
 #修改命令行
 cp .bashrc /root/.bashrc
 #刷新设置
 source ~/.bashrc
-```
 
-### apt安装软件
-```
+
+# apt安装软件
+
 apt install -y git 
 apt install -y tree 
 apt install -y htop 
@@ -71,16 +67,12 @@ apt install -y prodigal
 apt install -y awscli
 apt install -y sra-toolkit
 apt install -y speedtest-cli
-```
-### 网络测速
-安装完speedtest-cli可以直接用于网络测速
-```
+
+# 网络测速
 speedtest
-```
 
+# 安装bioconda
 
-### 安装bioconda
-```
 #下载bioconda
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 #安装
@@ -94,10 +86,10 @@ conda config --add channels bioconda
 
 #安装软件
 mamba install -y flye
-```
 
-### 打包conda环境
-```
+
+# 打包conda环境
+
 mamba create -n genome
 mamba install -n genome -y velvet
 mamba install -n genome -y flye
@@ -119,36 +111,35 @@ tar -zxvf genome.tar.gz -C genome
 #激活环境
 mamba activate genome
 source genome/bin/activate
-```
 
-### apptainer
-```
+# apptainer
+
 add-apt-repository -y ppa:apptainer/ppa
 apt update
 apt install -y apptainer
 apptainer pull docker://google/deepvariant
-```
-### 打包docker
-```
+
+# 打包docker
+
 apt install -y podman*
 docker pull google/deepvariant
 docker images 
 docker save -o 7d046533d9e9.tar docker.io/google/deepvariant:latest
 pigz 7d046533d9e9.tar 
-```
 
-### aws数据下载
-```
+
+# aws数据下载
+
 aws s3 cp --no-sign-request s3://sra-pub-run-odp/sra/SRR1039510/SRR1039510 SRR1039510.sra
-```
-### 下载数据
-```
+
+# 下载数据
+
 prefetch SRR8482586 -p
 prefetch SRR8494912 -p
 prefetch SRR8494939 -p
-```
-### 传输文件
-使用scp，sftp或者rsync等工具传到国内服务器上。
-```
-rsync -avP --rsh='ssh' 7d046533d9e9.tar.gz  abc123@xxx.xxx.xx.xx:~/
-```
+
+# 传输文件
+sftp abc123@xxx.xxx.xx.xx:~/
+scp -r file abc123@xxx.xxx.xx.xx:~/
+rsync -avP --rsh='ssh' file  abc123@xxx.xxx.xx.xx:~/
+
